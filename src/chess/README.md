e9517c8554d3b95a1a779f42f219f311f0ac941c
Justin Garbutt <jagarbut@squareup.com>
2021-12-22 22:33:28 -0800

    square map as an array but broken

--- src/chess/README.md
+++ src/chess/README.md
@@ -1,3 +1,11 @@
 # chess
 
 ## TODO
+
+12ms
+45ms
+525ms
+8761ms
+
+set 17.3%
+get 7.4%
--- src/chess/square-map.ts
+++ src/chess/square-map.ts
@@ -1,9 +1,119 @@
-import StringKeyMap from '../lib/string-key-map';
 import { Square } from './types';
-import { labelToSquare, squareLabel } from './utils';
 
-export class SquareMap<T> extends StringKeyMap<Square, T> {
+export class SquareMap<T> implements Map<Square, T> {
+  #map: T[][];
+
   constructor() {
-    super(squareLabel, labelToSquare);
+    this.#map = new Array<Array<T>>(
+      new Array<T>(8),
+      new Array<T>(8),
+      new Array<T>(8),
+      new Array<T>(8),
+      new Array<T>(8),
+      new Array<T>(8),
+      new Array<T>(8),
+      new Array<T>(8)
+    );
   }
+
+  clear(): void {
+    this.#map = new Array<Array<T>>(
+      new Array<T>(8),
+      new Array<T>(8),
+      new Array<T>(8),
+      new Array<T>(8),
+      new Array<T>(8),
+      new Array<T>(8),
+      new Array<T>(8),
+      new Array<T>(8)
+    );
+  }
+
+  delete(key: Square): boolean {
+    return delete this.#map[key.rank][key.file];
+  }
+
+  forEach(
+    callbackfn: (value: T, key: Square, map: Map<Square, T>) => void,
+    thisArg?: any
+  ): void {
+    throw new Error('Method not implemented.');
+  }
+
+  get(key: Square): T | undefined {
+    return this.#map[key.rank][key.file];
+  }
+
+  has(key: Square): boolean {
+    return Boolean(this.#map[key.rank][key.file]);
+  }
+
+  set(key: Square, value: T): this {
+    this.#map[key.rank][key.file] = value;
+    return this;
+  }
+
+  get size() {
+    let counter = 0;
+    for (let rank = 0; rank < 7; rank++) {
+      for (let file = 0; file < 7; file++) {
+        counter += this.#map[rank][file] === undefined ? 0 : 1;
+      }
+    }
+    return counter;
+  }
+
+  entries(): IterableIterator<[Square, T]> {
+    const map = this.#map;
+
+    return (function* () {
+      for (let rank = 0; rank < 7; rank++) {
+        for (let file = 0; file < 7; file++) {
+          const value = map[rank][file];
+
+          if (value) {
+            const tuple: [Square, T] = [{ rank, file }, value];
+            yield tuple;
+          }
+        }
+      }
+    })();
+  }
+
+  keys(): IterableIterator<Square> {
+    const map = this.#map;
+
+    return (function* () {
+      for (let rank = 0; rank < 7; rank++) {
+        for (let file = 0; file < 7; file++) {
+          const value = map[rank][file];
+
+          if (value) {
+            yield { rank, file };
+          }
+        }
+      }
+    })();
+  }
+
+  values(): IterableIterator<T> {
+    const map = this.#map;
+
+    return (function* () {
+      for (let rank = 0; rank < 7; rank++) {
+        for (let file = 0; file < 7; file++) {
+          const value = map[rank][file];
+          if (value) {
+            yield value;
+          }
+        }
+      }
+    })();
+  }
+
+  [Symbol.iterator](): IterableIterator<[Square, T]> {
+    return this.entries();
+  }
+
+  [Symbol.toStringTag]: string;
 }
