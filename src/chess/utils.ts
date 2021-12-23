0ab3c4619e88b4972ac6f30eefd82f8187440303
Justin Garbutt <jagarbut@squareup.com>
2021-12-22 23:10:58 -0800

    reimplement squareLabel using a lookup array, no dynamic strings

--- src/chess/utils.ts
+++ src/chess/utils.ts
@@ -14,13 +14,21 @@ export const ROOK_STARTING_SQUARES = Object.freeze({
   },
 });
 
+const SQUARE_LABEL_LOOKUP: SquareLabel[][] = [
+  ['a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1'],
+  ['a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2'],
+  ['a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3', 'h3'],
+  ['a4', 'b4', 'c4', 'd4', 'e4', 'f4', 'g4', 'h4'],
+  ['a5', 'b5', 'c5', 'd5', 'e5', 'f5', 'g5', 'h5'],
+  ['a6', 'b6', 'c6', 'd6', 'e6', 'f6', 'g6', 'h6'],
+  ['a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7'],
+  ['a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8'],
+];
+
 type Nullable<T> = T | undefined | null;
 
-const fileIndexToChar = (index: number): string =>
-  String.fromCharCode(index + 97);
-
 export const squareLabel = ({ rank, file }: Square): SquareLabel =>
-  `${fileIndexToChar(file)}${rank + 1}` as SquareLabel;
+  SQUARE_LABEL_LOOKUP[rank][file];
 
 export const labelToSquare = ([file, rank]: string): Square => ({
   file: file.charCodeAt(0) - 97,
