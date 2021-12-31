4ee782e043808dd9fb5a426f07b4696cc5306928
Justin Garbutt <jagarbut@squareup.com>
2021-12-30 23:57:22 -0800

    remove private props in engine, they are so slow so slow

--- src/chess/engine/index.ts
+++ src/chess/engine/index.ts
@@ -12,34 +12,34 @@ import { copyToInternal, copyToExternal } from './position';
 import { Position } from './types';
 
 export default class Engine {
-  #position: Position;
-  #moveStack: MoveResult[] = [];
+  _position: Position;
+  _moveStack: MoveResult[] = [];
 
   constructor(position: ExternalPosition) {
-    this.#position = copyToInternal(position);
+    this._position = copyToInternal(position);
   }
 
   applyMove(move: Move): Piece | undefined {
-    const result = applyMove(this.#position, move);
-    this.#moveStack.push(result);
+    const result = applyMove(this._position, move);
+    this._moveStack.push(result);
     return result.captured?.piece;
   }
 
   undoLastMove() {
-    const moveResult = this.#moveStack.pop();
+    const moveResult = this._moveStack.pop();
     if (moveResult) {
-      undoMove(this.#position, moveResult);
+      undoMove(this._position, moveResult);
     } else {
       throw Error('no last move to undo');
     }
   }
 
   evaluate(): number {
-    return evaluate(this.#position);
+    return evaluate(this._position);
   }
 
   evaluateNormalized(): number {
-    return this.#position.turn === Color.White
+    return this._position.turn === Color.White
       ? this.evaluate()
       : -1 * this.evaluate();
   }
@@ -50,20 +50,20 @@ export default class Engine {
 
   generateMovementData(): ComputedMovementData {
     return generateMovementData(
-      this.#position.pieces,
-      this.#position.turn,
-      this.#position,
+      this._position.pieces,
+      this._position.turn,
+      this._position,
       {
-        pinsToKing: this.#position.pinsToKing,
-        kings: this.#position.kings,
-        enPassantSquare: this.#position.enPassantSquare,
-        castlingAvailability: this.#position.castlingAvailability,
+        pinsToKing: this._position.pinsToKing,
+        kings: this._position.kings,
+        enPassantSquare: this._position.enPassantSquare,
+        castlingAvailability: this._position.castlingAvailability,
       }
     );
   }
 
   get position(): ExternalPosition {
-    return copyToExternal(this.#position);
+    return copyToExternal(this._position);
   }
 }
 
