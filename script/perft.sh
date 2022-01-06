8d19c974f10fe940d0b4c844ac6e2e823b4ff9ea
Justin Garbutt <jagarbut@squareup.com>
2022-01-06 10:37:19 -0800

    add shell script for running perft

---
+++ script/perft.sh
@@ -0,0 +1,1 @@
+node perft/dist/main.js $1 $2 $3
--- src/chess/perft.ts
+++ src/chess/perft.ts
@@ -1,3 +1,11 @@
 import { run, VIENNA_OPENING } from './lib/move-generation-perft';
+import { argv } from 'process';
+
+const DEBUG = true;
+
+const [, , depth, fen, moves] = argv;
+if (DEBUG) {
+  console.log('args', depth, fen, moves);
+}
 
 run(console.log, VIENNA_OPENING, 4);
