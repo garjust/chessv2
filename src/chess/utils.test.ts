59ea4caa80eaf7d6c2482c169cd3aaad904f9e7e
Justin Garbutt <jagarbut@gmail.com>
2023-08-25 16:54:57 -0700

    fix: final jest removal

--- src/chess/utils.test.ts
+++ src/chess/utils.test.ts
@@ -1,3 +1,4 @@
+import { expect, test } from 'vitest';
 import { DirectionUnit } from './types';
 import { directionOfMove } from './utils';
 
--- src/lib/timer.test.ts
+++ src/lib/timer.test.ts
@@ -1,17 +1,18 @@
 import { expect, test } from 'vitest';
 import Timer from './timer';
+import { vi } from 'vitest';
 
 test('timer works', () => {
-  jest.useFakeTimers();
-  jest.spyOn(global, 'setInterval');
+  vi.useFakeTimers();
+  vi.spyOn(global, 'setInterval');
 
   const timer = new Timer(100, { tickRate: 10, autoStart: false, debug: true });
   timer.start();
   expect(setInterval).toHaveBeenCalledTimes(1);
   expect(timer.brrring()).toEqual(false);
 
-  jest.runAllTimers();
+  vi.runAllTimers();
   expect(timer.brrring()).toEqual(true);
 
-  jest.useRealTimers();
+  vi.useRealTimers();
 });
