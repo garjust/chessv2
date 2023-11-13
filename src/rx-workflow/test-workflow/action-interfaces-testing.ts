1ed2c3771593275c8154243447d8595ff5b36686
Justin Garbutt <jagarbut@gmail.com>
2023-11-13 11:12:33 -0800

    chore: ANOTHER BREAKTHROUGH yess

--- src/rx-workflow/test-workflow/action-interfaces-testing.ts
+++ src/rx-workflow/test-workflow/action-interfaces-testing.ts
@@ -26,38 +26,51 @@ const fooAction = (val: number) =>
     type: Type.Foo,
     val,
   }) as const;
-type FooAction = ReturnType<typeof fooAction>;
+// type FooAction = ReturnType<typeof fooAction>;
 
 const barAction = (val: string) =>
   ({
     type: Type.Bar,
     val,
   }) as const;
-type BarAction = ReturnType<typeof barAction>;
+// type BarAction = ReturnType<typeof barAction>;
 
 const catAction = (val: boolean) =>
   ({
     type: Type.Cat,
     val,
   }) as const;
-type CatAction = ReturnType<typeof catAction>;
+// type CatAction = ReturnType<typeof catAction>;
 
-type Action = FooAction | BarAction | CatAction;
+// type Action = FooAction | BarAction | CatAction;
+type Action =
+  | ReturnType<typeof fooAction>
+  | ReturnType<typeof barAction>
+  | ReturnType<typeof catAction>;
 
 // rest of workflow...
 
 type State = { counter: number };
 
-function handleFoo(state: State, action: FooAction): Update<State, Action> {
+function handleFoo(
+  state: State,
+  action: Action & { type: Type.Foo },
+): Update<State, Action> {
   return [{ ...state, counter: state.counter + action.val }, null];
 }
 
-function handleBar(state: State, action: BarAction): Update<State, Action> {
+function handleBar(
+  state: State,
+  action: Action & { type: Type.Bar },
+): Update<State, Action> {
   console.log('bar!', action.val, state.counter);
   return [state, null];
 }
 
-function handleCat(state: State, action: CatAction): Update<State, Action> {
+function handleCat(
+  state: State,
+  action: Action & { type: Type.Cat },
+): Update<State, Action> {
   console.log('is a cat?', action.val);
   return [state, null];
 }
