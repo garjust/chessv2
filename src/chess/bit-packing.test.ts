3e4fb5807af689605456baf2b6b48f8a4b78c079
Justin Garbutt <jagarbut@gmail.com>
2023-11-17 12:36:41 -0800

    feat: add promotion to intMove and add intPiece

--- src/chess/bit-packing.test.ts
+++ src/chess/bit-packing.test.ts
@@ -1,9 +1,14 @@
 import { expect, test } from 'vitest';
 import { moveInt, unpackMove } from './bit-packing';
-import { Color, PieceType } from './types';
+import { Color, PieceType, Square } from './types';
 
 test('moveInt', () => {
-  const move = moveInt(Color.White, PieceType.Knight, 20, 35);
+  const move = moveInt(
+    Color.White,
+    PieceType.Knight,
+    20 as Square,
+    35 as Square,
+  );
   expect(move).toEqual(0b01000110101000100);
 
   const unpacked = unpackMove(move);
@@ -11,4 +16,23 @@ test('moveInt', () => {
   expect(unpacked.pieceType).toEqual(PieceType.Knight);
   expect(unpacked.from).toEqual(20);
   expect(unpacked.to).toEqual(35);
+  expect(unpacked.promotion).toEqual(null);
+});
+
+test('moveInt with promotion', () => {
+  const move = moveInt(
+    Color.White,
+    PieceType.Knight,
+    20 as Square,
+    35 as Square,
+    PieceType.Queen,
+  );
+  expect(move).toEqual(0b1011000110101000100);
+
+  const unpacked = unpackMove(move);
+  expect(unpacked.color).toEqual(Color.White);
+  expect(unpacked.pieceType).toEqual(PieceType.Knight);
+  expect(unpacked.from).toEqual(20);
+  expect(unpacked.to).toEqual(35);
+  expect(unpacked.promotion).toEqual(PieceType.Queen);
 });
--- src/chess/bit-packing.ts
+++ src/chess/bit-packing.ts
@@ -1,32 +1,47 @@
 import { Color, PieceType, Square } from './types';
 
-export const COLOR_MASK = 0b1;
-export const PIECE_MASK = 0b1110;
-export const FROM_MASK = 0b1111110000;
-export const TO_MASK = 0b1111110000000000;
-
 export const PIECE_SHIFT = 1;
-export const FROM_SHIFT = 4;
-export const TO_SHIFT = 10;
+export const FROM_SHIFT = PIECE_SHIFT + 3;
+export const TO_SHIFT = FROM_SHIFT + 6;
+export const PROMOTION_SHIFT = TO_SHIFT + 6;
+
+export const COLOR_MASK = 0b1;
+export const PIECE_MASK = 0b111 << PIECE_SHIFT;
+export const FROM_MASK = 0b111111 << FROM_SHIFT;
+export const TO_MASK = 0b111111 << TO_SHIFT;
+export const PROMOTION_MASK = 0b111 << PROMOTION_SHIFT;
 
 type intMove = Brand<number, 'intMove'>;
+type intPiece = Brand<number, 'intPiece'>;
 
 export const moveInt = (
   color: Color,
   pieceType: PieceType,
   from: Square,
   to: Square,
+  promotion?: PieceType,
 ): intMove =>
   (color |
     (pieceType << PIECE_SHIFT) |
     (from << FROM_SHIFT) |
-    (to << TO_SHIFT)) as intMove;
+    (to << TO_SHIFT) |
+    ((promotion ?? 0) << PROMOTION_SHIFT)) as intMove;
 
-export const unpackMove = (
-  move: intMove,
-): { color: Color; pieceType: PieceType; from: Square; to: Square } => ({
-  color: move & COLOR_MASK,
-  pieceType: (move & PIECE_MASK) >>> PIECE_SHIFT,
-  from: (move & FROM_MASK) >>> FROM_SHIFT,
-  to: (move & TO_MASK) >>> TO_SHIFT,
-});
+export const unpackMove = (move: intMove) =>
+  ({
+    color: move & COLOR_MASK,
+    pieceType: (move & PIECE_MASK) >>> PIECE_SHIFT,
+    from: ((move & FROM_MASK) >>> FROM_SHIFT) as Square,
+    to: ((move & TO_MASK) >>> TO_SHIFT) as Square,
+    promotion:
+      (move & PROMOTION_MASK) >>> PROMOTION_SHIFT !== 0
+        ? (((move & PROMOTION_MASK) >>> PROMOTION_SHIFT) as PieceType)
+        : null,
+  }) as const;
+
+export const pieceInt = (
+  color: Color,
+  pieceType: PieceType,
+  square: Square,
+): intPiece =>
+  moveInt(color, pieceType, square, 0 as Square) as number as intPiece;
