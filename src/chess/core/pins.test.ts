31f1eca430dc6419ee01a9e195410e12d5213463
Justin Garbutt <jagarbut@gmail.com>
2023-11-26 15:57:25 -0800

    refactor: use Core in pins test

--- src/chess/core/pins.test.ts
+++ src/chess/core/pins.test.ts
@@ -7,15 +7,10 @@ import Core from '.';
 
 test('pins', () => {
   // Vienna gambit accepted with moreee pinsss.
-  const position = parseFEN(
-    '2k2bnr/pppr1p2/2n3p1/3NPB1p/5Bb1/5N2/PPP3PP/R2K3R w  - 2 14',
+  const core = new Core(
+    parseFEN('2k2bnr/pppr1p2/2n3p1/3NPB1p/5Bb1/5N2/PPP3PP/R2K3R w  - 2 14'),
   );
-  const absolutePins = {
-    [Color.White]: new Pins(position.pieces, 3, Color.White),
-    [Color.Black]: new Pins(position.pieces, 58, Color.Black),
-  };
-
-  expect(absolutePins[Color.White].allPins).toEqual([
+  expect(core.pins).toEqual([
     {
       to: 21,
       from: 30,
@@ -28,8 +23,6 @@ test('pins', () => {
       direction: DirectionUnit.Down,
       legalMoveSquares: [11, 19, 27, 43, 35],
     },
-  ]);
-  expect(absolutePins[Color.Black].allPins).toEqual([
     {
       to: 51,
       from: 37,
