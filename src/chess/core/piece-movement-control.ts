935e0787f735f6dbcffd135a824bf8f0f2569cb4
Justin Garbutt <jagarbut@gmail.com>
2023-11-27 01:32:54 -0800

    refactor: condense forPiece

--- src/chess/core/piece-movement-control.ts
+++ src/chess/core/piece-movement-control.ts
@@ -9,57 +9,29 @@ import {
 } from './lookup';
 import { rayControlScanner } from './move-utils';
 
-export const pawnMoves = (piece: Piece, from: Square): SquareControl[] =>
-  PAWN_CAPTURE_MOVES[piece.color][from];
-
-export const knightMoves = (from: Square, piece: Piece): SquareControl[] =>
-  KNIGHT_MOVES[piece.color][from];
-
-export const kingMoves = (from: Square, piece: Piece): SquareControl[] =>
-  KING_MOVES[piece.color][from];
-
-export const bishopMoves = (
-  pieces: Map<Square, Piece>,
-  piece: Piece,
-  from: Square,
-): SquareControl[] =>
-  BISHOP_RAYS[from].flatMap((ray) =>
-    rayControlScanner(pieces, piece, from, ray),
-  );
-
-export const rookMoves = (
-  pieces: Map<Square, Piece>,
-  piece: Piece,
-  from: Square,
-): SquareControl[] =>
-  ROOK_RAYS[from].flatMap((ray) => rayControlScanner(pieces, piece, from, ray));
-
-export const queenMoves = (
-  pieces: Map<Square, Piece>,
-  piece: Piece,
-  from: Square,
-): SquareControl[] =>
-  QUEEN_RAYS[from].flatMap((ray) =>
-    rayControlScanner(pieces, piece, from, ray),
-  );
-
 export const forPiece = (
   piece: Piece,
   pieces: Map<Square, Piece>,
   square: Square,
 ): SquareControl[] => {
   switch (piece.type) {
-    case PieceType.Bishop:
-      return bishopMoves(pieces, piece, square);
-    case PieceType.King:
-      return kingMoves(square, piece);
-    case PieceType.Knight:
-      return knightMoves(square, piece);
     case PieceType.Pawn:
-      return pawnMoves(piece, square);
-    case PieceType.Queen:
-      return queenMoves(pieces, piece, square);
+      return PAWN_CAPTURE_MOVES[piece.color][square];
+    case PieceType.King:
+      return KING_MOVES[piece.color][square];
+    case PieceType.Knight:
+      return KNIGHT_MOVES[piece.color][square];
+    case PieceType.Bishop:
+      return BISHOP_RAYS[square].flatMap((ray) =>
+        rayControlScanner(pieces, piece, square, ray),
+      );
     case PieceType.Rook:
-      return rookMoves(pieces, piece, square);
+      return ROOK_RAYS[square].flatMap((ray) =>
+        rayControlScanner(pieces, piece, square, ray),
+      );
+    case PieceType.Queen:
+      return QUEEN_RAYS[square].flatMap((ray) =>
+        rayControlScanner(pieces, piece, square, ray),
+      );
   }
 };
