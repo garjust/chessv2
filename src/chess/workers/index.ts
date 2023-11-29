dae588fdb463774c835074bc0643b32511ebc171
Justin Garbutt <jagarbut@gmail.com>
2023-11-28 16:37:10 -0800

    refactor: final touchup fully consolidate worker code

--- src/chess/workers/index.ts
+++ src/chess/workers/index.ts
@@ -1,34 +1,74 @@
-import type { Remote } from 'comlink';
-import type Timer from '../../lib/timer';
-import type { SearchExecutor } from '../engine/search-executor';
+import { Endpoint, Remote, wrap } from 'comlink';
+import nodeEndpoint from 'comlink/src/node-adapter';
+import { Worker as NodeWorker } from 'node:worker_threads';
+import Logger from '../../lib/logger';
+import Timer from '../../lib/timer';
+import { SearchExecutor } from '../engine/search-executor';
+
+const logger = new Logger('worker-init');
 
 export const loadPerftWorker = async (): Promise<
-  [engine: Worker, cleanup: () => void]
+  [worker: Worker, cleanup: () => void]
 > => {
-  const { loadPerft: load } = await import('./webworker');
-  return load();
+  const worker = new Worker(new URL('./perft', import.meta.url), {
+    type: 'module',
+  });
+  worker.addEventListener('error', (event) => {
+    logger.error('perft web worker crashed', event.message, event);
+  });
+  return [worker, () => worker.terminate()];
 };
 
 export const loadSearchExecutorWorker = async (
   ...args: ConstructorParameters<typeof SearchExecutor>
-): Promise<[engine: Remote<SearchExecutor>, cleanup: () => void]> => {
+): Promise<[executor: Remote<SearchExecutor>, cleanup: () => void]> => {
+  logger.debug('loading search-executor web worker');
+  let worker: Worker | NodeWorker;
+  let endpoint: Endpoint;
+
   if (USE_NODE_WORKER_THREAD) {
-    const { loadSearchExecutor: load } = await import('./webworker');
-    return load(...args);
+    worker = new NodeWorker(new URL('./search-executor.mjs', import.meta.url));
+    endpoint = nodeEndpoint(worker);
   } else {
-    const { loadSearchExecutor: load } = await import('./webworker');
-    return load(...args);
+    worker = new Worker(new URL('./search-executor', import.meta.url), {
+      type: 'module',
+    });
+    endpoint = worker;
+    endpoint.addEventListener('error', (event) => {
+      logger.error(`search-executor web worker crashed`, event);
+    });
   }
+  const RemoteClass = wrap<typeof SearchExecutor>(endpoint);
+
+  logger.debug('creating remote SearchExecutor instance');
+  const instance = await new RemoteClass(...args);
+  logger.debug('created remote SearchExecutor instance');
+  return [instance, () => worker.terminate()];
 };
 
 export const loadTimerWorker = async (
   ...args: ConstructorParameters<typeof Timer>
-): Promise<[engine: Remote<Timer>, cleanup: () => void]> => {
+): Promise<[timer: Remote<Timer>, cleanup: () => void]> => {
+  logger.debug('loading timer web worker');
+  let worker: Worker | NodeWorker;
+  let endpoint: Endpoint;
+
   if (USE_NODE_WORKER_THREAD) {
-    const { loadTimer: load } = await import('./webworker');
-    return load(...args);
+    worker = new NodeWorker(new URL('./timer.mjs', import.meta.url));
+    endpoint = nodeEndpoint(worker);
   } else {
-    const { loadTimer: load } = await import('./webworker');
-    return load(...args);
+    worker = new Worker(new URL('./timer', import.meta.url), {
+      type: 'module',
+    });
+    endpoint = worker;
+    endpoint.addEventListener('error', (event) => {
+      logger.error(`timer web worker crashed`, event);
+    });
   }
+  const RemoteClass = wrap<typeof Timer>(endpoint);
+
+  logger.debug('creating remote Timer instance');
+  const instance = await new RemoteClass(...args);
+  logger.debug('created remote Timer instance');
+  return [instance, () => worker.terminate()];
 };
