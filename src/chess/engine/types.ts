267743e321e018b0c4c3ae4e5ea31bef47168eb3
Justin Garbutt <jagarbut@gmail.com>
2023-12-06 16:52:31 -0500

    refactor: NodeType is an integer const enum

--- src/chess/engine/types.ts
+++ src/chess/engine/types.ts
@@ -29,16 +29,16 @@ export type SearchResult = {
 };
 
 // See documentation here https://www.chessprogramming.org/Node_Types.
-export enum NodeType {
+export const enum NodeType {
   // A PV node is a node where the best score alpha < X < beta. Therefore the
   // best move for this node becomes a part of the PV.
-  PV = 'PV',
+  PV,
   // A cut node is one where beta was exceeded (score X >= beta) and therefore
   // no further nodes were searched in the branch.
-  Cut = 'CUT',
+  Cut,
   // An all node is a node in which the best score X < alpha. All moves at this
   // were searched and a good move was not found.
-  All = 'ALL',
+  All,
 }
 
 export type TranspositionTableEntry = {
