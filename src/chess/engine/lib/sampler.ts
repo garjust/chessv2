a47f39327be93ba2d7c4fefe028af26e38f894a5
Justin Garbutt <jagarbut@gmail.com>
2023-12-25 13:03:34 -0700

    refactor: combine sample.ts sampler.ts

--- src/chess/engine/lib/sampler.ts
+++ src/chess/engine/lib/sampler.ts
@@ -1,5 +1,24 @@
 import TimerSync from '../../../lib/timer-sync';
 
+// /** Communication with web workers is slow, too slow to do at every node.
+//  *
+//  * Instead of asking the timer if it has reached 0 at every node we instead
+//  * will only check the actual timer once per X ms. X should be larger than or
+//  * equal to the timer tick rate.
+//  */
+// const TIMER_SAMPLE_RATE = 200;
+
+// /** Make an assumption about how long it takes to process a node in the search
+//  * tree.
+//  */
+// const MICROSECONDS_PER_NODE = 50;
+
+// /** Caculate a threshold using the assumption for μs/node and the sample rate.
+//  * We also assume the timer is read exactly once per node.
+//  */
+// const TIMER_SAMPLE_THRESHOLD =
+//   (TIMER_SAMPLE_RATE * 1000) / MICROSECONDS_PER_NODE;
+
 /**
  * Manages a "sample rate" to periodically report diagnostic info about an
  * ongoing search.
@@ -20,7 +39,7 @@ export default class Sampler {
       sampleNodes: number;
     } = {
       sampleMs: 1000,
-      sampleNodes: 25000,
+      sampleNodes: 50000,
     },
   ) {
     this.timer = new TimerSync(sampleMs);
