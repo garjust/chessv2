4b60f1515473cb540bb66574bce195b1e5feeecf
Justin Garbutt <jagarbut@gmail.com>
2023-12-26 11:19:26 -0700

    chore: fix documentation in engine typeS

--- src/chess/engine/types.ts
+++ src/chess/engine/types.ts
@@ -20,7 +20,9 @@ export interface SearchConstructor {
   new (infoReporter: InfoReporter): SearchInterface;
 }
 
-// Object returned from the search function
+/**
+ * Object returned from the search function.
+ */
 export type SearchResult = {
   move: Move;
   pv: Move[];
@@ -28,32 +30,44 @@ export type SearchResult = {
   scores: { move: Move; score: number }[];
 };
 
-// See documentation here https://www.chessprogramming.org/Node_Types.
+/**
+ *  See documentation here https://www.chessprogramming.org/Node_Types.
+ */
 export const enum NodeType {
-  // A PV node is a node where the best score alpha < X < beta. Therefore the
-  // best move for this node becomes a part of the PV.
+  /**
+   * A PV node is a node where the best score alpha < X < beta. Therefore the
+   * best move for this node becomes a part of the PV.
+   */
   PV,
-  // A cut node is one where beta was exceeded (score X >= beta) and therefore
-  // no further nodes were searched in the branch.
+  /*
+   * A cut node is one where beta was exceeded (score X >= beta) and therefore
+   * no further nodes were searched in the branch.
+   */
   Cut,
-  // An all node is a node in which the best score X < alpha. All moves at this
-  // were searched and a good move was not found.
+  /*
+   * An all node is a node in which the best score X < alpha. All moves at this
+   * node were searched and a good move was not found.
+   */
   All,
 }
 
 export type TranspositionTableEntry = {
   nodeType: NodeType;
-  // The depth of search which resulted in this entry. For example when entries
-  // are created at the second ply of an N-depth search this value will be
-  // equal to N-1. Lower values mean more plys were searched to determine the
-  // score.
+  /**
+   * The depth of search which resulted in this entry. For example when entries
+   * are created at the second ply of an N-depth search this value will be
+   * equal to N-1. Lower values mean more plys were searched to determine the
+   * score.
+   */
   inverseDepth: number;
-  // This is the score returned by the search function when this entry was
-  // made.
-  // - When the node type is CUT this represents a lower-bound for the score
-  // of the position and can be re-tested against beta values.
-  // - When the node type is PV this represents an exact value for the score.
-  // - When the node type is ALL this represents an upper-bound for the score.
+  /**
+   * This is the score returned by the search function when this entry was
+   * made.
+   * - When the node type is CUT this represents a lower-bound for the score
+   * of the position and can be re-tested against beta values.
+   * - When the node type is PV this represents an exact value for the score.
+   * - When the node type is ALL this represents an upper-bound for the score.
+   */
   score: number;
   move?: Move;
 };
