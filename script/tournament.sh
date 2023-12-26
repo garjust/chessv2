6d6c36d83350f7f627fac86261a0dcdb2f80ee75
Justin Garbutt <jagarbut@gmail.com>
2023-12-25 17:17:30 -0700

    fix: tournament gives margin to engine move time

--- script/tournament.sh
+++ script/tournament.sh
@@ -1,6 +1,7 @@
 vendor/cutechess-cli \
-  -engine name=alphabeta cmd=./vendor/engine-builds/alphabeta.v0/engine.mjs st=1 proto=uci \
-  -engine name=iterative cmd=./vendor/engine-builds/iterative.v0/engine.mjs st=1 proto=uci \
+  -engine name=alphabeta cmd=./vendor/engine-builds/alphabeta.v0/engine.mjs st=1 timemargin=50 proto=uci \
+  -engine name=iterative cmd=./vendor/engine-builds/iterative.v0/engine.mjs st=1 timemargin=50 proto=uci \
   -rounds 2 -games 2 \
   -outcomeinterval 1 \
+  -concurrency 1 \
   -openings file=./vendor/silver-50.pgn -debug
