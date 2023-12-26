cc5c6dff663c0499e85446a25cf5c047ea362bf1
Justin Garbutt <jagarbut@gmail.com>
2023-12-25 17:49:09 -0700

    chore: update tournament to be a 50 round run

--- script/tournament.sh
+++ script/tournament.sh
@@ -1,7 +1,11 @@
+rm engine-results.pgn
+
 vendor/cutechess-cli \
-  -engine name=iterative-weak cmd=./vendor/engine-builds/iterative-weak.v0/engine.mjs st=0.5 timemargin=50 proto=uci \
   -engine name=iterative cmd=./vendor/engine-builds/iterative.v0/engine.mjs st=0.5 timemargin=50 proto=uci \
-  -rounds 2 -games 2 \
+  -engine name=iterative-weak cmd=./vendor/engine-builds/iterative-weak.v0/engine.mjs st=0.5 timemargin=50 proto=uci \
+  -rounds 50 -games 2 \
+  -repeat 2 \
   -outcomeinterval 1 \
   -concurrency 4 \
-  -openings file=./vendor/silver-50.pgn
+  -openings file=./vendor/silver-50.pgn \
+  -pgnout engine-results.pgn min
