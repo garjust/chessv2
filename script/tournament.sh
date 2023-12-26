0a9bf8230a523ded5a34d064839e953e5bded8ee
Justin Garbutt <jagarbut@gmail.com>
2023-12-25 17:22:28 -0700

    chore: update tournament

--- script/tournament.sh
+++ script/tournament.sh
@@ -1,7 +1,7 @@
 vendor/cutechess-cli \
-  -engine name=alphabeta cmd=./vendor/engine-builds/iterative-weak.v0/engine.mjs st=1 timemargin=50 proto=uci \
-  -engine name=iterative cmd=./vendor/engine-builds/iterative.v0/engine.mjs st=1 timemargin=50 proto=uci \
+  -engine name=iterative-weak cmd=./vendor/engine-builds/iterative-weak.v0/engine.mjs st=0.5 timemargin=50 proto=uci \
+  -engine name=iterative cmd=./vendor/engine-builds/iterative.v0/engine.mjs st=0.5 timemargin=50 proto=uci \
   -rounds 2 -games 2 \
   -outcomeinterval 1 \
-  -concurrency 1 \
-  -openings file=./vendor/silver-50.pgn -debug
+  -concurrency 4 \
+  -openings file=./vendor/silver-50.pgn
